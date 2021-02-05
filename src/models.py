785249e8025778c2957db0b6b3f96d867a29efb0
luciano588 <luciano588.alm@gmail.com>
2021-02-05 22:58:46 +0000

    01

--- src/models.py
+++ src/models.py
@@ -8,23 +8,28 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    post_text = Column(String(250), nullable=False)
+    user_id = Column (Integer, ForeignKey('user.id'))
+    media = relationship ("Media", back_populates="post")
+    comments = relationship("Comment", back_populates="post")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Comment(Base):
+    __tablename__ = 'comment    '
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment = Column(String(250), nullable=False)
+    author = relationship("user", back_populates="post", lazy=True)
+    post_id = Column (Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column (Integer, ForeignKey('post.id'))
+    url = Column(String(250), nullable=False)
+    media_type = Column(String(250), nullable=False)
+    
 
     def to_dict(self):
         return {}
